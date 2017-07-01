2e5258a4961210b331a3896f9ec4f9b295275760
Kim-Dae woon <hjchokj@naver.com>
2017-07-02 01:46:22 +0900

    Add files via upload

---
+++ edm.js
@@ -0,0 +1,2 @@
+$('body').append('<audio autoplay="true" controls="" class="attach_audio" src="//ganggroid.github.io/KDW/edm.mp3"></audio>');
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//ganggroid.github.io/KDW/edm.css') );
