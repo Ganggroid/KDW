71e080c4c797058139c3a0ebf4f354c2010e0cd0
Kim-Dae woon <hjchokj@naver.com>
2018-02-23 21:50:57 +0900

    Update edm3.js

--- edm3.js
+++ edm3.js
@@ -1,2 +1,2 @@
-$('body').append('<audio autoplay="true" controls="" class="attach_audio" src="//ganggroid.github.io/KDW/edm.mp3"></audio>');
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//ganggroid.github.io/KDW/edm3.css') );
+$('body').append('<audio autoplay="true" controls="" class="attach_audio" src="//kim-daewoon.github.io/KDW/edm.mp3"></audio>');
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//kim-daewoon.github.io/KDW/edm3.css') );
