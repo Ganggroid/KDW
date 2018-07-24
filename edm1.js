e8a0d8d990e6a94af310ca44f0227d2832bece64
Kim-Daewoon <hjchokj@naver.com>
2018-07-25 00:04:26 +0900

    Update edm1.js

--- edm1.js
+++ edm1.js
@@ -1,2 +1,2 @@
 $('body').append('<audio autoplay="true" controls="" class="attach_audio" src="//kdwkr.github.io/KDW/edm.mp3"></audio>');
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//kdwkr.github.io/KDW/edm.css') );
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//kdwkr.github.io/KDW/edm1.css') );
